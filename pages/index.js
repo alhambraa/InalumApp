1ea7333288233c68c77513fd9ab071f2aa82d3ca
alhambraa <hambra02@gmail.com>
2020-12-13 10:57:49 +0700

    Update Project

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 /* eslint-disable import/prefer-default-export */
 import Splash from './Splash';
 import Login from './Login';
+import Home from './Home';
+import Cart from './Cart';
 
 export {
   Splash,
   Login,
+  Home,
+  Cart,
 };
